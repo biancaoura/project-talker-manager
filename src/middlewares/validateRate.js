b57d23e70ed5e7984ce0eac7cb2f8fb713832762
Bianca Oura <biancaoura@gmail.com>
2022-11-05 15:18:36 -0300

    corrige ortografia

--- src/middlewares/validateRate.js
+++ src/middlewares/validateRate.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
   }
 
   if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 a 5' });
   }
 
   next();
