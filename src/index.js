91cdb0ba558a4116071a500fc92b90be1de6ae5c
Bianca Oura <biancaoura@gmail.com>
2022-11-04 22:59:56 -0300

    cria endpoint GET /talker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { readTalkers } = require('./readFile');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,12 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await readTalkers();
+  if (talkers) {
+    return res.status(200).json(talkers);
+  }
+  return res.status(200).json([]);
 });
