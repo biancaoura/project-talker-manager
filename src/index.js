32cfd0ef4a37f70e0b0948f2e0dd803ed25be3fa
Bianca Oura <biancaoura@gmail.com>
2022-11-04 23:47:56 -0300

    cria endpoint POST /login e gera token

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const talkerRoutes = require('./routes/talkerRoutes');
+const loginRoute = require('./routes/loginRoute');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/talker', talkerRoutes);
+app.use('/login', loginRoute);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
